49d73d58ab98d434c4d144258702fb687c01d806
guy <guydvir.tech@gmail.com>
2023-04-22 04:58:40 +0300

    1st

--- sendTelegram/main.py
+++ sendTelegram/main.py
@@ -1,13 +1,14 @@
 import requests
 import datetime
 import pywhatkit
+import time
 
 chatID = '596123373'
 BOT_TOKEN_1 = "812406965:AAEaV-ONCIru8ePuisuMfm0ECygsm5adZHs"  # botbot
 BOT_TOKEN_2 = "497268459:AAESYm27tJfNXwnnnn0slbmWnkqvbWgQEyw"  # homepi_bot
 BOT_TOKEN_3 = "1238925698:AAGWARuQ2eyx2i0ui2sCp_mM7xInTsKgUuM"  # guyd_test2bot
 
-tokens = [BOT_TOKEN_1, BOT_TOKEN_2, BOT_TOKEN_3]
+# tokens = [BOT_TOKEN_1, BOT_TOKEN_2, BOT_TOKEN_3]
 
 
 def send_to_telegram(message):
@@ -21,3 +22,4 @@ def send_to_telegram(message):
 
 send_to_telegram(datetime.datetime.now().ctime())
 pywhatkit.sendwhatmsg("+972-524291167", "hi", 21, 36)
+time.sleep(30)
